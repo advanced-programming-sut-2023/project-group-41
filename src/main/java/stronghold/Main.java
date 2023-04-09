7ea1d422f55a002aefcf4168035784d2c9ac1ffd
Mani <mani.ebra@gmail.com>
2023-04-10 00:38:39 +0330

    added json output handler for sign up menu

--- src/main/java/stronghold/Main.java
+++ src/main/java/stronghold/Main.java
@@ -1,10 +1,18 @@
 package stronghold;
 
+import com.google.gson.*;
 import stronghold.view.MyJavaFXApp;
+import stronghold.view.SignUpMenuView;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+
+
+    public static void main(String[] args){
         MyJavaFXApp.main(args);
     }
 }
---
+++ src/main/java/stronghold/view/SignUpMenuView.java
@@ -0,0 +1,33 @@
+package stronghold.view;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SignUpMenuView {
+    public static void output(String code, Object... params){
+        try {
+            JsonElement jsonElement = JsonParser.parseReader(new FileReader("src/main/java/stronghold/database/SignUpMenuResponses.json"));
+            String response = String.valueOf(jsonElement.getAsJsonObject().get(code));
+            response = response.substring(1,response.length()-1);
+            for (int i = 1; i <= Arrays.stream(params).count(); i++) {
+                String toBeReplaced = "\\$VAR" + i + "\\$";
+                response = response.replaceAll(toBeReplaced, (String) params[i - 1]);
+            }
+            response = response.replaceAll("\\\\n","\n");
+            System.out.println(response);
+        } catch (
+                Error |
+                FileNotFoundException e){
+        }
+    }
+    public static void input(Scanner scanner){
+        if(!scanner.hasNextLine())
+            return;
+        String readLine = scanner.nextLine();
+    }
+}
