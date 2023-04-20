0aa1d7554dd18cc89812638376f5dd3958972b6b
mmd.jef <119729533+MohammadJafaripour@users.noreply.github.com>
2023-04-20 21:10:26 +0330

    Resource added

---
+++ src/main/java/stronghold/model/components/game/Resource.java
@@ -0,0 +1,18 @@
+package stronghold.model.components.game;
+
+public enum Resource {
+    GOLD,
+    WHEAT,
+    FLOUR,
+    HOPS,
+    ALE,
+    STONE,
+    IRON,
+    WOOD,
+    PITCH,
+    ARMOR,
+    SWORD,
+    HORSE,
+    ROSS_BOW,
+    COW;
+}
