d98359ffc2357c3f6b36ea2489ab2c110b2ba5ef
mmd.jef <119729533+MohammadJafaripour@users.noreply.github.com>
2023-04-20 20:11:14 +0330

    Texture added

---
+++ src/main/java/stronghold/model/components/game/Texture.java
@@ -0,0 +1,20 @@
+package stronghold.model.components.game;
+
+public enum Texture {
+    LAND,
+    GRAVEL_LAND,
+    ROCK,
+    STONE,
+    IRON,
+    GRASS,
+    MEADOW,
+    DENSE_GRASS_LAND,
+    OIL,
+    PLAIN,
+    SHALLOW_LAKE,
+    RIVER,
+    SMALL_POND,
+    BIG_POND,
+    BEACH,
+    SEA;
+}
