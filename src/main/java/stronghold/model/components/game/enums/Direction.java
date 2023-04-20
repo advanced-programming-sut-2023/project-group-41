c61518659fca7e59c718b83588e44bc0dafa5b3b
mmd.jef <119729533+MohammadJafaripour@users.noreply.github.com>
2023-04-20 20:12:47 +0330

    Tree and Direction added

---
+++ src/main/java/stronghold/model/components/game/enums/Direction.java
@@ -0,0 +1,9 @@
+package stronghold.model.components.game.enums;
+
+public enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+    RANDOM;
+}
---
+++ src/main/java/stronghold/model/components/game/enums/Tree.java
@@ -0,0 +1,9 @@
+package stronghold.model.components.game.enums;
+
+public enum Tree {
+    DESERT_SHRUB,
+    CHERRY_PALM,
+    OLIVE_TREE,
+    COCONUT_PALM,
+    DATE;
+}
