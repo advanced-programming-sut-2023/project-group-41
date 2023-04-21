c4e6a4fa8c7d2ccb59de45f49b9bc34353c698a6
mmd.jef <119729533+MohammadJafaripour@users.noreply.github.com>
2023-04-21 11:19:10 +0330

    convert String to enum added to class

--- src/main/java/stronghold/model/components/game/Resource.java
+++ src/main/java/stronghold/model/components/game/Resource.java
@@ -1,18 +1,49 @@
 package stronghold.model.components.game;
 
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum Resource {
-    GOLD,
-    WHEAT,
-    FLOUR,
-    HOPS,
-    ALE,
-    STONE,
-    IRON,
-    WOOD,
-    PITCH,
-    ARMOR,
-    SWORD,
-    HORSE,
-    ROSS_BOW,
-    COW;
+    GOLD("gold"),
+
+    WHEAT("wheat"),
+    FLOUR("flour"),
+    HOPS("hops"),
+    ALE("ale"),
+    STONE("stone"),
+    IRON("iron"),
+    WOOD("wood"),
+    PITCH("pitch"),
+
+    ARMOR("armor"),
+    SWORD("sword"),
+    HORSE("horse"),
+    ROSS_BOW("ross bow"),
+
+    COW("cow");
+
+    private String regex;
+
+    public static final ArrayList<Resource> resources = new ArrayList<>(EnumSet.allOf(Resource.class));
+
+    Resource(String regex) {
+        this.regex = regex;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public static Matcher getMatcher(String input, Resource resource){
+        return Pattern.compile(resource.getRegex()).matcher(input);
+    }
+
+    public static Resource getResource(String input){
+        for (Resource resource : resources) {
+            if (getMatcher(input, resource).find()) return resource;
+        }
+        return null;
+    }
 }
--- src/main/java/stronghold/model/components/game/Texture.java
+++ src/main/java/stronghold/model/components/game/Texture.java
@@ -1,20 +1,49 @@
 package stronghold.model.components.game;
 
+import stronghold.model.components.game.enums.Food;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum Texture {
-    LAND,
-    GRAVEL_LAND,
-    ROCK,
-    STONE,
-    IRON,
-    GRASS,
-    MEADOW,
-    DENSE_GRASS_LAND,
-    OIL,
-    PLAIN,
-    SHALLOW_LAKE,
-    RIVER,
-    SMALL_POND,
-    BIG_POND,
-    BEACH,
-    SEA;
+    LAND("land"),
+    GRAVEL_LAND("gravel land"),
+    ROCK("rock"),
+    STONE("stone"),
+    IRON("iron"),
+    GRASS("grass"),
+    MEADOW("meadow"),
+    DENSE_GRASS_LAND("dense grass land"),
+    OIL("oil"),
+    PLAIN("plain"),
+    SHALLOW_LAKE("shallow lake"),
+    RIVER("river"),
+    SMALL_POND("small pond"),
+    BIG_POND("big pond"),
+    BEACH("beach"),
+    SEA("sea");
+
+    private String regex;
+    public static final ArrayList<Texture> textureArr = new ArrayList<>(EnumSet.allOf(Texture.class));
+
+    Texture(String regex) {
+        this.regex = regex;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public static Matcher getMatcher(String input, Texture texture){
+        return Pattern.compile(texture.getRegex()).matcher(input);
+    }
+
+    public static Texture getTexture(String input) {
+        for (Texture texture : textureArr) {
+            if(getMatcher(input, texture).find()) return texture;
+        }
+        return null;
+    }
 }
