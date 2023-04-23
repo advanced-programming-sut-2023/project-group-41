017f2ab417be64de8b5e16d5d594678783c7f613
Mohiedd <mohammad8313amin@gmail.com>
2023-04-23 17:29:44 +0430

    debugged some wrong merged files

--- src/main/java/stronghold/model/components/game/building/Building.java
+++ src/main/java/stronghold/model/components/game/building/Building.java
@@ -1,67 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 package stronghold.model.components.game.building;
 
 import stronghold.model.components.game.enums.Resource;
-
-import java.util.ArrayList;
-
-public abstract class Building {
-    protected int health;
-    protected int cost;
-    protected int workerNum;
-    protected boolean engineerWorkers;
-    protected ArrayList<Resource> neededResources;
-    protected int populationEffect;
-    protected int popularityEffect;
-
-    public Building(int health, int cost, int workerNum, boolean engineerWorkers,
-                    ArrayList<Resource> neededResources, int populationEffect, int popularityEffect) {
-        this.health = health;
-        this.cost = cost;
-        this.workerNum = workerNum;
-        this.engineerWorkers = engineerWorkers;
-        this.neededResources = neededResources;
-        this.populationEffect = populationEffect;
-        this.popularityEffect = popularityEffect;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public int getCost() {
-        return cost;
-    }
-
-    public int getWorkerNum() {
-        return workerNum;
-    }
-
-    public boolean isEngineerWorkers() {
-        return engineerWorkers;
-    }
-
-    public ArrayList<Resource> getNeededResources() {
-        return neededResources;
-    }
-
-    public int getPopulationEffect() {
-        return populationEffect;
-    }
-
-    public int getPopularityEffect() {
-        return popularityEffect;
-    }
-}
-=======
-=======
-
->>>>>>> 2c354c3e141bdb26633c22d637da4e87f73c5938
-package stronghold.model.components.game.building;
-
-import stronghold.model.components.game.Resource;
-import stronghold.model.components.game.People;
 
 import java.util.ArrayList;
 
