2818c0603f86aac81ec3acbcf31dab0b68d246f9
mehrAd <mjafaripoursws@gmail.com>
2023-05-03 20:16:49 +0330

    government added to Building

--- src/main/java/stronghold/model/components/game/building/Building.java
+++ src/main/java/stronghold/model/components/game/building/Building.java
@@ -1,9 +1,12 @@
 package stronghold.model.components.game.building;
 
 
+import stronghold.model.components.game.Government;
 import stronghold.model.components.game.enums.Resource;
+import stronghold.model.components.general.User;
 
 public abstract class Building {
+    protected Government ownership;
     protected int health;
     protected int cost;
     protected int workerNum;
@@ -11,13 +14,18 @@ public abstract class Building {
     protected Resource neededResource;
     protected int neededResourceCount;
 
-    public Building(int health,int cost, int workerNum, boolean engineerWorkers, Resource neededResources, int neededResourceCount) {
+    public Building(Government ownership, int health,int cost, int workerNum, boolean engineerWorkers, Resource neededResources, int neededResourceCount) {
+        this.ownership = ownership;
         this.health = health;
         this.cost = cost;
         this.workerNum = workerNum;
         this.engineerWorkers = engineerWorkers;
         this.neededResource = neededResources;
         this.neededResourceCount = neededResourceCount;
+    }
+
+    public Government getOwnership() {
+        return ownership;
     }
 
     public int getHealth() {
@@ -38,17 +46,17 @@ public abstract class Building {
     }
     public int getNeededResourceCount() { return neededResourceCount;}
 
-    public static Building getBuilding(String input){
+    public static Building getBuilding(Government ownership, String input){
         if (CastleType.getCastleType(input) != null)
-            return new Castle(CastleType.getCastleType(input));
+            return new Castle(ownership, CastleType.getCastleType(input));
         else if (ConverterType.getConverterType(input) != null)
-            return new Converter(ConverterType.getConverterType(input));
+            return new Converter(ownership, ConverterType.getConverterType(input));
         else if (DevelopmentType.getDevelopmentType(input) != null)
-            return new Development(DevelopmentType.getDevelopmentType(input));
+            return new Development(ownership, DevelopmentType.getDevelopmentType(input));
         else if (ResourceMakerType.getResourceMakerType(input) != null)
-            return new ResourceMaker(ResourceMakerType.getResourceMakerType(input));
+            return new ResourceMaker(ownership, ResourceMakerType.getResourceMakerType(input));
         else if (StorageType.getStorageType(input) != null)
-            return new Storage(StorageType.getStorageType(input));
+            return new Storage(ownership, StorageType.getStorageType(input));
         else
             return null;
     }
