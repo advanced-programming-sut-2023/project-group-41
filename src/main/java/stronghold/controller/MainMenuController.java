49e1e66c5e40cf12c0cde927af192e72a1f19bdd
mehrAd <mjafaripoursws@gmail.com>
2023-05-09 00:04:43 +0330

    fixing menus

--- src/main/java/stronghold/controller/MainMenuController.java
+++ src/main/java/stronghold/controller/MainMenuController.java
@@ -3,13 +3,13 @@ package stronghold.controller;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import stronghold.model.components.game.Government;
 import stronghold.model.components.game.Map;
+import stronghold.model.components.game.Unit;
 import stronghold.model.components.game.building.Building;
-import stronghold.model.components.game.enums.Direction;
-import stronghold.model.components.game.enums.State;
-import stronghold.model.components.game.enums.Texture;
-import stronghold.model.components.game.enums.Tree;
 import stronghold.model.components.general.User;
+import stronghold.model.database.UsersDB;
+import stronghold.view.GameMenuView;
 import stronghold.view.MainMenuView;
 
 import java.io.FileNotFoundException;
@@ -17,13 +17,9 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
+
 public class MainMenuController extends MenuController{
-    private ArrayList<User> userDataBase;
-    private User currentUser;
-    private static int roundNum;
-    private static Map map;
     private static String pathToRegexJSON = "src/main/java/stronghold/database/utils/regex/MainMenuRegex.json";
-    
     public static void run(User currentUser, Scanner scanner) {
         JsonElement regexElement = null;
         try {
@@ -32,11 +28,29 @@ public class MainMenuController extends MenuController{
                 FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        JsonObject mainMenuRegexObj = regexElement.getAsJsonObject();
-
-        MainMenuView.output("welcome", (Object) currentUser.getUsername());
+        JsonObject MainMenuRegexObj = regexElement.getAsJsonObject();
         while (true) {
-
+            String command = MainMenuView.input(scanner).trim();
+            Matcher startGameMatcher;
+            if (command.matches("user\\s+logout")) {
+                MainMenuView.output("logout");
+                break;
+            } else if ((startGameMatcher = getJSONRegexMatcher(command, "startGame", MainMenuRegexObj)).matches()){
+                MainMenuView.output("enterUsers");
+                ArrayList<User> users = new ArrayList<>();
+                while (true){
+                    String input = MainMenuView.input(scanner).trim();
+                    if (input.matches("FINISH")){
+                        break;
+                    } else{
+                        users.add(UsersDB.usersDB.getUserByUsername(input));
+                        //Todo: handel uncorrected user names
+                    }
+                }
+                GameMenuController.run(currentUser.getGovernment(), users, scanner, 1);
+            } else{
+                MainMenuView.output("invalid");
+            } // TODO: adding if statement for entering profile menu
         }
     }
 }
