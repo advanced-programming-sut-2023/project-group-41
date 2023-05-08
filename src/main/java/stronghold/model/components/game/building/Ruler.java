285368cdd8240d419aad956bd9f7cbb252a35de0
mehrAd <mjafaripoursws@gmail.com>
2023-05-09 00:10:40 +0330

    adding ruler

---
+++ src/main/java/stronghold/model/components/game/building/Ruler.java
@@ -0,0 +1,19 @@
+package stronghold.model.components.game.building;
+
+import stronghold.model.components.game.Government;
+
+public class Ruler extends  Building {
+
+    private Government owner;
+
+    public Ruler(Government owner){
+            super(owner,500,0,0,false,null,0);
+
+
+    }
+
+    @Override
+    public String getRegex() {
+        return null;
+    }
+}
