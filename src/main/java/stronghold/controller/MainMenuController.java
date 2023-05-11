e43ad0157b0ba13010907591fe21a2105518806c
Mani <mani.ebra@gmail.com>
2023-05-11 14:32:37 +0330

    linked the first 3 menus and fixed get JSON error

--- src/main/java/stronghold/controller/MainMenuController.java
+++ src/main/java/stronghold/controller/MainMenuController.java
@@ -14,6 +14,8 @@ import stronghold.view.MainMenuView;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -21,6 +23,7 @@ import java.util.regex.Matcher;
 public class MainMenuController extends MenuController{
     private static String pathToRegexJSON = "src/main/java/stronghold/database/utils/regex/MainMenuRegex.json";
     public static void run(User currentUser, Scanner scanner) {
+        
         JsonElement regexElement = null;
         try {
             regexElement = JsonParser.parseReader(new FileReader(pathToRegexJSON));
@@ -31,11 +34,32 @@ public class MainMenuController extends MenuController{
         JsonObject MainMenuRegexObj = regexElement.getAsJsonObject();
         while (true) {
             String command = MainMenuView.input(scanner).trim();
-            Matcher startGameMatcher;
+            Matcher startGameMatcher = getJSONRegexMatcher(command, "startNewGame", MainMenuRegexObj);
             if (command.matches("user\\s+logout")) {
                 MainMenuView.output("logout");
+                JsonElement prefsElement;
+                String pathToPrefs = "src/main/java/stronghold/database/datasets/preferences.json";
+                try {
+                    prefsElement = JsonParser.parseReader(
+                            new FileReader(pathToPrefs));
+                } catch (
+                        FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+
+                try {
+                    String toBeWritten = prefsElement.toString();
+                    toBeWritten = toBeWritten.replace(currentUser.getUsername(),"!NULLUSER");
+                    FileWriter fileWriter = new FileWriter(pathToPrefs);
+                    fileWriter.write(toBeWritten);
+                    fileWriter.close();
+                } catch (
+                        IOException e) {
+                    throw new RuntimeException(e);
+                }
+                SignUpMenuController.run(scanner);
                 break;
-            } else if ((startGameMatcher = getJSONRegexMatcher(command, "startGame", MainMenuRegexObj)).matches()){
+            } else if (startGameMatcher.matches()){
                 MainMenuView.output("enterUsers");
                 ArrayList<User> users = new ArrayList<>();
                 while (true){
@@ -44,7 +68,7 @@ public class MainMenuController extends MenuController{
                         break;
                     } else{
                         users.add(UsersDB.usersDB.getUserByUsername(input));
-                        //Todo: handel uncorrected user names
+                        //TODO: handle uncorrected user names
                     }
                 }
                 GameMenuController.run( scanner, 1,1,1);
