260ac9adf4c9e1f5b5d11c07e082723ed4063760
Mani <mani.ebra@gmail.com>
2023-06-24 10:29:56 +0330

    fixed module-info.java and opened graphical controllers to FXML

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,12 @@ module stronghold {
     requires javafx.fxml;
     requires com.google.gson;
 
+    opens stronghold.controller.graphical to javafx.fxml;
+
     exports stronghold.view to javafx.graphics;
     exports stronghold to javafx.graphics;
     exports stronghold.view.graphics to javafx.graphics;
     exports stronghold.model.components.game.graphical to javafx.graphics;
     exports stronghold.controller to javafx.fxml;
-    exports stronghold.controller.graphical to javafx.fxml;
+    exports stronghold.controller.graphical to javafx.fxml, javafx.graphics;
 }
--- src/main/java/stronghold/controller/graphical/RegisterController.java
+++ src/main/java/stronghold/controller/graphical/RegisterController.java
@@ -1,16 +1,20 @@
 package stronghold.controller.graphical;
 
-import javafx.collections.SetChangeListener;
+import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import stronghold.controller.MenuController;
-
-import java.util.EventListener;
 
 public class RegisterController extends MenuController {
     public TextField usernameField;
 
-    public void onUsernameFieldChange(EventListener eventListener){
-        System.out.println(1);
 
+    public void onUsernameFieldChange(ActionEvent inputMethodEvent) {
+        System.out.println(1);
+    }
+
+    public void sampleFunc(MouseEvent mouseEvent) {
+        System.out.println(27);
+        
     }
 }
