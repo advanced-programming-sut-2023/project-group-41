cd1a208772bd0056758fadccfd958903f9c89a4c
Mani <mani.ebra@gmail.com>
2023-06-30 15:47:44 +0330

    fixed network test for register view

--- src/main/java/stronghold/networktest/HostTest.java
+++ src/main/java/stronghold/networktest/HostTest.java
@@ -2,12 +2,11 @@ package stronghold.networktest;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import stronghold.model.components.general.User;
-import stronghold.model.database.UsersDB;
 import stronghold.model.utils.network.seth.Host;
 import stronghold.view.graphics.RegisterView;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class HostTest extends Application {
 
@@ -18,11 +17,15 @@ public class HostTest extends Application {
     }
     public static void main(String[] args) throws IOException {
         Host host = new Host();
+        AtomicBoolean hasBeenLaunched = new AtomicBoolean(false);
         host.setHandleReceivedObjects(object -> {
             if(object instanceof RegisterView){
                 RegisterView registerView = (RegisterView) object;
                 try {
-                    registerView.start(new Stage());
+                    if(!hasBeenLaunched.get()){
+                        registerView.start(new Stage());
+                        hasBeenLaunched.set(true);
+                    }
                 } catch (
                         Exception e) {
                     throw new RuntimeException(e);
